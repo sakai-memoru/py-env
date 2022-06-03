4309045f465e9dab5ede4946d8af14aeb76d90e2
sakai-memoru <sakai.memoru@gmail.com>
2022-06-03 16:44:47 +0900

    first commit

---
+++ batch.py
@@ -0,0 +1,11 @@
+import sys
+
+## ---------------------- ## process
+def process(argv):
+    print("Hello, python!")
+
+
+## ---------------------- ## entry point
+if __name__ == "__main__":
+    process(sys.argv)
+
